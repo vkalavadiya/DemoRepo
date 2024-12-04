dcae20e16933ec8df7233d89508eff0d0b724899
hp <vishva2573@gmail.com>
2024-12-04 13:47:28 +0530

    new Commit

--- src/main/java/com/example/demo2/student/AddNumber.java
+++ src/main/java/com/example/demo2/student/AddNumber.java
@@ -4,7 +4,6 @@ public class AddNumber {
     public static void main(String []args){
         int x = 30;
         int y = 20;
-        System.out.println(x+y);
-        System.out.println(x-y);
+        System.out.println(x*y);
     }
 }
