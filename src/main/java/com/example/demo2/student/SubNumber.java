cab8cd8801158d5742ab3bf1f6a5b5bcf14fc097
hp <vishva2573@gmail.com>
2024-12-04 12:43:04 +0530

    add new Commit

--- src/main/java/com/example/demo2/student/SubNumber.java
+++ src/main/java/com/example/demo2/student/SubNumber.java
@@ -4,6 +4,7 @@ public class SubNumber {
     public static void main(String []args){
         int a=20;
         int b=10;
-        System.out.println(a-b);
+        int c=a-b;
+        System.out.println(c);
     }
 }
