bf9e8b5433f64a7c0f049e8ed917c72692c9abd2
hp <vishva2573@gmail.com>
2024-12-04 12:29:16 +0530

    add Commit

---
+++ src/main/java/com/example/demo2/student/Numbers.java
@@ -0,0 +1,9 @@
+package com.example.demo2.student;
+
+public class Numbers {
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+}
