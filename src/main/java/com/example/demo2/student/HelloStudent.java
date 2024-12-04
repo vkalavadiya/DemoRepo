abcec898b4b1f8495aaba29018b381e311cd2a29
hp <vishva2573@gmail.com>
2024-12-04 11:58:09 +0530

    add Commit

--- src/main/java/com/example/demo2/student/HelloStudent.java
+++ src/main/java/com/example/demo2/student/HelloStudent.java
@@ -3,5 +3,6 @@ package com.example.demo2.student;
 public class HelloStudent {
     public static void main(String []args){
         System.out.println("Helloo Student");
+        System.out.println("Hello from here");
     }
 }
