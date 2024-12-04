53f965499b05bf55e09a4c029759174b18863b5b
hp <vishva2573@gmail.com>
2024-12-04 12:33:48 +0530

    add Commit

---
+++ src/main/java/com/example/demo2/student/SubNumber.java
@@ -0,0 +1,9 @@
+package com.example.demo2.student;
+
+public class SubNumber {
+    public static void main(String []args){
+        int a=20;
+        int b=10;
+        System.out.println(a-b);
+    }
+}
