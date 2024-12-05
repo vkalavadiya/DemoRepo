5310d02299518c255bf4e221049476aef4bc1081
hp <vishva2573@gmail.com>
2024-12-05 09:53:17 +0530

    new Commit

---
+++ src/main/java/com/example/demo2/student/NewStudent.java
@@ -0,0 +1,8 @@
+package com.example.demo2.student;
+
+public class NewStudent {
+    public static void main(String []args){
+        System.out.println("My Name is ABC");
+        System.out.println("I am from Rajkot");
+    }
+}
