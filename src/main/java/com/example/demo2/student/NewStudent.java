62ccb6609f8a93b35837b67d42a4cdb9fa60bc87
hp <vishva2573@gmail.com>
2024-12-05 09:54:42 +0530

    new Commit

--- src/main/java/com/example/demo2/student/NewStudent.java
+++ src/main/java/com/example/demo2/student/NewStudent.java
@@ -4,5 +4,6 @@ public class NewStudent {
     public static void main(String []args){
         System.out.println("My Name is ABC");
         System.out.println("I am from Rajkot");
+        System.out.println("India is my country");
     }
 }
