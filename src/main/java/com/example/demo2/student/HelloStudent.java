f89c9b0e27b70cfd9a8115f52bb0a3a4746a1658
hp <vishva2573@gmail.com>
2024-12-05 10:02:17 +0530

    new Commit

--- src/main/java/com/example/demo2/student/HelloStudent.java
+++ src/main/java/com/example/demo2/student/HelloStudent.java
@@ -3,7 +3,5 @@ package com.example.demo2.student;
 public class HelloStudent {
     public static void main(String []args){
         System.out.println("Helloo Student");
-        System.out.println("Hello from here");
-        System.out.println("Hello from Rajkot");
     }
 }
